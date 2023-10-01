3e9b5c3cd4498cf0a1968a9dbe6838eb34b1ac95
ArchiComputer <itmekjs@gmail.com>
2023-09-30 21:45:30 -0600

    semi-colon

--- udp_server.c
+++ udp_server.c
@@ -114,9 +114,9 @@ int main(int argc, char **argv) {
       error("ERROR in sendto");
 
     // Check if the received message is "exit"
-    printf("buf : ", buf)
-    printf(strcmp(buf, "exit") == 0)
-    printf(strcmp(buf, "exit"))
+    printf("buf : ", buf);
+    printf(strcmp(buf, "exit") == 0);
+    printf(strcmp(buf, "exit"));
     if (strcmp(buf, "exit") == 0) {
       printf("Received 'exit' message. Server is shutting down.\n");
       break; // Exit the loop and close the server
